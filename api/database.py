df7bf148ace4384f92ce082f198e2c3aaae8bcd5
OpexRah <dougisrave@gmail.com>
2024-12-24 14:11:25 +0530

    feat: add db env vars

--- api/database.py
+++ api/database.py
@@ -1,8 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1234567890@localhost/snapsorter" # change to env var later
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@localhost/{DB_NAME}"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 session_local = sessionmaker(autoflush=False, bind=engine)
 
