4a7f049da911b9ea11583018c2ff3c4c7e568ab1
OpexRah <dougisrave@gmail.com>
2024-12-26 14:08:35 +0530

    feat: remove email pass login

--- api/routers/auth.py
+++ api/routers/auth.py
@@ -1,11 +1,10 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Request
 from fastapi.responses import RedirectResponse
-from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from authlib.integrations.starlette_client import OAuth
 from starlette.config import Config
 from ..database import get_db
-from .. import db_models, utils, oauth2, response_schemas
+from .. import db_models, oauth2
 
 router = APIRouter(
     tags = ["Auth"]
@@ -21,18 +20,6 @@ google = oauth.register(
     client_kwargs={"scope": "openid email profile"},
 )
 
-@router.post("/login", response_model=response_schemas.Token)
-def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    user = db.query(db_models.User).filter(db_models.User.email == user_credentials.username).first()
-    if not user:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials")
-    
-    if not utils.verify(user_credentials.password, user.password):
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials")
-    
-    # create a token and return it
-    access_token = oauth2.create_access_token(data = {"user_id" : user.id})
-    return {"access_token" : access_token, "token_type" : "bearer"}
     
 @router.get("/google-login")
 async def google_login(request: Request):
