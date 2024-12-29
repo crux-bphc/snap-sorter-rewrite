4db37b4f88778e50f32cc968cc3128a1d8730e2a
Swastik Goswami <cwswastik2005@gmail.com>
2024-12-29 21:38:55 +0530

    feat: auth

---
+++ frontend/src/components/redirect-handler.tsx
@@ -0,0 +1,24 @@
+import { useAuth } from "./auth-context";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+const RedirectHandler: React.FC = () => {
+  const { setToken } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const token = params.get("token");
+
+    if (token) {
+      setToken(token);
+      navigate("/");
+    } else {
+      console.error("No token found in URL");
+    }
+  }, [setToken, navigate]);
+
+  return <p>Redirecting...</p>;
+};
+
+export default RedirectHandler;
