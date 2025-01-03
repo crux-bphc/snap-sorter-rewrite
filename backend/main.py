69724abe64fb894008fb7a345c02fa9c23a4bbab
Swastik Goswami <cwswastik2005@gmail.com>
2025-01-03 11:00:15 +0530

    fix: cors

--- backend/main.py
+++ backend/main.py
@@ -20,6 +20,7 @@ app.mount("/images", StaticFiles(directory="data/images"), name="images")
 origins = [
     "http://localhost:8080",
     "http://127.0.0.1:5173",
+    "http://localhost:5173",
 ]
 
 app.add_middleware(
