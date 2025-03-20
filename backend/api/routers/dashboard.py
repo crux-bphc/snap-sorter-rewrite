b3779d65372927bc77ec61b0d95589d5198faecd
OpexRah <dougisrave@gmail.com>
2025-03-20 19:10:07 +0530

    feat(backend): add stats endpoint

--- backend/api/routers/dashboard.py
+++ backend/api/routers/dashboard.py
@@ -21,3 +21,27 @@ async def get_announcements(
     response["announcements"] = [announcement.announcement for announcement in announcements_object]
 
     return response
+
+
+@router.get("/stats", response_model=response_schemas.StatsResponse)
+async def get_stats(
+    db: Session = Depends(get_db),
+):
+    """
+    Returns stats
+    """
+    users = db.query(db_models.User).count()
+    images = db.query(db_models.Image).count()
+    events = db.query(db_models.Event).count()
+    inference_count = db.query(db_models.UserFaceAndResult).count()
+    zip_files = db.query(db_models.ZipFileRecord).count()
+
+    response = {
+        "total_users": users,
+        "total_images": images,
+        "total_events": events,
+        "inference_count": inference_count,
+        "zip_files": zip_files
+    }
+
+    return response
